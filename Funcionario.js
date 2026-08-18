f1e67110cbeb439df79d897a3980c844fbcd96b0
alunocbra <alunocbra@ifb.local>
2026-08-18 17:19:24 -0300

    Adiciona a Classe Abstrata Funcionario

---
+++ Funcionario.js
@@ -0,0 +1,29 @@
+export class Funcionario {
+    nome;
+    idFuncional;
+    #salario;
+
+    constructor(nome, idFuncional, salario){
+
+        if(this.constructor === Funcionario){
+            throw new Error("Classe abstrata 'Funcionario' não pode ser instanciada.")
+        }
+
+        this.nome = nome;
+        this.idFuncional = idFuncional;
+        this.#salario = salario;
+    }
+
+    get salario(){
+        return this.#salario 
+    }
+
+    set salario(novoSalario){
+        this.#salario = novoSalario 
+    }
+
+    descreverFuncao() {
+        throw new Error("O método descreverFuncao() deve ser implementado pela classe filha.")
+}
+}
+
